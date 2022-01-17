00a8c75eb49516f48b70977ce6c84a6c9991f6a6
leba-gd <luca.ebach@gdata-adan.de>
2022-01-17 17:39:25 +0100

    improve srum parser

--- kuiper/app/parsers/srum_parser/srum_interface.py
+++ kuiper/app/parsers/srum_parser/srum_interface.py
@@ -3,20 +3,20 @@ import srum
 
 def SRUM_interface(file, parser):
     try:
-		srum_obj = srum.SRUM_Parser( file )
-		return_data = []
-		if srum_obj.ApplicationResourceUsage is not None:
-			return_data += srum_obj.ApplicationResourceUsage
-		if srum_obj.ApplicationResourceUsage is not None:
-			return_data += srum_obj.NetworkDataUsageMonitor
-		if srum_obj.ApplicationResourceUsage is not None:
-			return_data += srum_obj.NetworkConnectivityUsageMonitor
-		return return_data
+        srum_obj = srum.SRUM_Parser( file )
+        return_data = []
+        if srum_obj.ApplicationResourceUsage is not None:
+           return_data += srum_obj.ApplicationResourceUsage
+        if srum_obj.ApplicationResourceUsage is not None:
+           return_data += srum_obj.NetworkDataUsageMonitor
+        if srum_obj.ApplicationResourceUsage is not None:
+           return_data += srum_obj.NetworkConnectivityUsageMonitor
+        return return_data
 
     except Exception as e:
-    	exc_type, exc_obj, exc_tb = sys.exc_info()
-    	msg = "[-] [Error] " + parser + " Parser: " + str(exc_obj) + " - Line No. " + str(exc_tb.tb_lineno)
-    	return (None , msg)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        msg = "[-] [Error] " + parser + " Parser: " + str(exc_obj) + " - Line No. " + str(exc_tb.tb_lineno)
+        return (None , msg)
 
 
 
